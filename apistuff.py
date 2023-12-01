712543b08d809bbee79c169618560ead99320d0f
gagnonl03 <gagnonl@msoe.edu>
2023-11-30 18:08:11 -0600

    things

--- apistuff.py
+++ apistuff.py
@@ -1,3 +1,27 @@
+import csv
+
 import helper
 
-helper.save_json("temp", helper.get_pgcr("7306552831"))
+
+def write_player_to_csv(data):
+    with open("out.csv", "a") as my_csv:
+        csvWriter = csv.writer(my_csv, delimiter=",")
+        csvWriter.writerows(data)
+
+
+test = helper.get_pgcr("7305790649")
+helper.save_json("alec", test)
+with open("out.csv", "a") as my_csv:
+    csvWriter = csv.writer(my_csv, delimiter=",")
+    csvWriter.writerow(["Name", "Time Played", "Class", "Light", "Kills", "Deaths"])
+
+for item in test["Response"]["entries"]:
+    out = helper.get_player_stats(item)
+    print(len(out))
+    print(out)
+    temp = [out]
+    write_player_to_csv(temp)
+
+with open("out.csv", "a") as my_csv:
+    csvWriter = csv.writer(my_csv, delimiter=",")
+    csvWriter.writerow(["END GROUP"])
--- helper.py
+++ helper.py
@@ -19,3 +19,34 @@ def make_request(url):
 def get_pgcr(pgcr_id):
     temp_url = base_url + f"/Destiny2/Stats/PostGameCarnageReport/{pgcr_id}"
     return make_request(temp_url).json()
+
+
+def get_player_playtime(id, type):
+    temp_url = base_url + f"/Destiny2/{type}/Profile/{id}/?components=200"
+    data = make_request(temp_url).json()
+    save_json("check", data)
+    minutes_played_total = 0
+    for item in data["Response"]["characters"]["data"]:
+        minutes_played_total += int(data["Response"]["characters"]["data"][item]["minutesPlayedTotal"])
+
+    return minutes_played_total
+
+
+def get_player_stats(data):
+    player_data = data["player"]
+    player_name = player_data["destinyUserInfo"]["displayName"]
+    player_memberID = player_data["destinyUserInfo"]["membershipId"]
+    player_memberType = player_data["destinyUserInfo"]["membershipType"]
+    player_time_played = get_player_playtime(player_memberID, player_memberType)
+    player_class = player_data["characterClass"]
+    player_light = player_data["lightLevel"]
+    player_kills = data["values"]["kills"]["basic"]["value"]
+    player_deaths = data["values"]["deaths"]["basic"]["value"]
+    player_kdr = data["values"]["killsDeathsRatio"]["basic"]["value"]
+    player_kda = data["values"]["killsDeathsAssists"]["basic"]["value"]
+    player_grenade = data["extended"]["values"]["weaponKillsGrenade"]["basic"]["value"]
+    player_melee = data["extended"]["values"]["weaponKillsMelee"]["basic"]["value"]
+    player_super = data["extended"]["values"]["weaponKillsSuper"]["basic"]["value"]
+    return [player_name, player_time_played, player_class, player_light, player_kills, player_deaths,
+            player_kdr, player_kda, player_grenade, player_melee, player_super]
+
