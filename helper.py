ca1402e5024c457b808aff9e640b71f4e8a91a55
slooooth <115724866+gagnonl03@users.noreply.github.com>
2023-11-30 20:58:18 -0600

    wrap try except

--- helper.py
+++ helper.py
@@ -33,21 +33,37 @@ def get_player_playtime(id, type):
 
 
 def get_player_stats(data):
-    player_data = data["player"]
-    player_name = player_data["destinyUserInfo"]["displayName"]
-    player_memberID = player_data["destinyUserInfo"]["membershipId"]
-    player_memberType = player_data["destinyUserInfo"]["membershipType"]
-    player_time_played = get_player_playtime(player_memberID, player_memberType)
-    player_class = player_data["characterClass"]
-    player_light = player_data["lightLevel"]
-    player_kills = data["values"]["kills"]["basic"]["value"]
-    player_deaths = data["values"]["deaths"]["basic"]["value"]
-    player_kdr = data["values"]["killsDeathsRatio"]["basic"]["value"]
-    player_kda = data["values"]["killsDeathsAssists"]["basic"]["value"]
-    player_grenade = data["extended"]["values"]["weaponKillsGrenade"]["basic"]["value"]
-    player_melee = data["extended"]["values"]["weaponKillsMelee"]["basic"]["value"]
-    player_super = data["extended"]["values"]["weaponKillsSuper"]["basic"]["value"]
-    completed_activity = data["values"]["completed"]["basic"]["value"]
+    try:
+        player_data = data["player"]
+        player_name = player_data["destinyUserInfo"]["displayName"]
+        player_memberID = player_data["destinyUserInfo"]["membershipId"]
+        player_memberType = player_data["destinyUserInfo"]["membershipType"]
+        player_time_played = get_player_playtime(player_memberID, player_memberType)
+        player_class = player_data["characterClass"]
+        player_light = player_data["lightLevel"]
+        player_kills = data["values"]["kills"]["basic"]["value"]
+        player_deaths = data["values"]["deaths"]["basic"]["value"]
+        player_kdr = data["values"]["killsDeathsRatio"]["basic"]["value"]
+        player_kda = data["values"]["killsDeathsAssists"]["basic"]["value"]
+        player_grenade = data["extended"]["values"]["weaponKillsGrenade"]["basic"]["value"]
+        player_melee = data["extended"]["values"]["weaponKillsMelee"]["basic"]["value"]
+        player_super = data["extended"]["values"]["weaponKillsSuper"]["basic"]["value"]
+        completed_activity = data["values"]["completed"]["basic"]["value"]
+    except:
+        player_name = "dummy"
+        player_memberID = "dummy"
+        player_memberType = "dummy"
+        player_time_played = "dummy"
+        player_class = "dummy"
+        player_light = "dummy"
+        player_kills = "dummy"
+        player_deaths = "dummy"
+        player_kdr = "dummy"
+        player_kda = "dummy"
+        player_grenade = "dummy"
+        player_melee = "dummy"
+        player_super = "dummy"
+        completed_activity = "dummy"
     return [player_name, player_time_played, player_class, player_light, player_kills, player_deaths,
             player_kdr, player_kda, player_grenade, player_melee, player_super, completed_activity]
 
