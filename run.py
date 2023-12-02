9fc32daa238cb88922d23b8afba7de389defb2fe
slooooth <115724866+gagnonl03@users.noreply.github.com>
2023-12-02 14:30:20 -0600

    fixes

--- run.py
+++ run.py
@@ -6,10 +6,10 @@ import apistuff
 # RUN INSTRUCTIONS::
 
 # Change this to the name you want to save all the data to (im already running crota)
-output_name = "vowplayers2.csv"
+output_name = "dscplayers.csv"
 
 # Rename this to the name file you want to read pcgrs from
-df = pd.read_csv("vowofthedisciple.csv", header=None)
+df = pd.read_csv("deepstonecrypt.csv", header=None)
 df.columns = ["pcgr", "time"]
 
 with open(output_name, "a", encoding="utf-8") as my_csv:
